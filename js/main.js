61e42d629f7925fa4fe647b5cf57b8462cf963c1
GurinYI <Gurin.ucheba@gmail.com>
2022-07-19 23:10:54 +0500

    задание 9 правки

--- js/main.js
+++ js/main.js
@@ -21,15 +21,15 @@ const getRandomPositiveIntegerWithDecimalPlaces = (min, max, decimalPlaces) => {
   return result;
 };
 
-const getRandomArrayElement = (elements) => {
+function getRandomArrayElement(elements) {
   return elements[getRandomPositiveInteger(0, elements.length - 1)];
-};
+}
 
 const getArrayWithNoRepeat = (elements) => {
-  let randomArray = [];
-  let randomArrayLength = getRandomPositiveInteger(1, elements.length);
+  const randomArray = [];
+  const randomArrayLength = getRandomPositiveInteger(1, elements.length);
   while (randomArray.length < randomArrayLength) {
-    let randomIndex = getRandomPositiveInteger(0, elements.length - 1);
+    const randomIndex = getRandomPositiveInteger(0, elements.length - 1);
     if (!randomArray.includes(elements[randomIndex])) {
       randomArray.push(elements[randomIndex]);
     }
@@ -37,66 +37,74 @@ const getArrayWithNoRepeat = (elements) => {
   return randomArray;
 };
 
-let unusedId = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const UNUSED_ID = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const getImageId = () => {
-  imageId = getRandomPositiveInteger(1, 10);
-  let index = unusedId.indexOf(imageId);
-  imageId < 10 ? (imageId = "0" + imageId) : (imageId += "");
-  if (index >= 0) {
-    unusedId.splice(index, 1);
+  const imageId = getRandomPositiveInteger(0, UNUSED_ID.length - 1);
+  const index = UNUSED_ID.indexOf(UNUSED_ID[imageId]);
+  let imageLink = '';
+  if (UNUSED_ID[imageId] < 10) {
+    imageLink =`0${UNUSED_ID[imageId]}`;
+  } else {
+    imageLink = `${UNUSED_ID[imageId]}`;
   }
-  console.log(unusedId);
-  return imageId;
+  if(index >= 0) {
+    UNUSED_ID.splice(index,1);
+  }
+  return imageLink;
 };
-const TYPES = ["palace", "flat", "house", "bungalow", "hotel"];
+
+
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 
 const PHOTOS = [
-  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg",
-  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg",
-  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg",
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 
-const CHECKINS = ["12:00", "13:00", "14:00"];
+const CHECKINS = ['12:00', '13:00', '14:00'];
 
-const CHECKOUTS = ["12:00", "13:00", "14:00"];
+const CHECKOUTS = ['12:00',' 13:00', '14:00'];
 
 const FEATURES = [
-  "wifi",
-  "dishwasher",
-  "parking",
-  "washer",
-  "elevator",
-  "conditioner",
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
 ];
 
-const AUTHOR = {
-  avatar: "img/avatars/user" + getImageId() + ".png",
-};
-const LOCATION = {
-  lat: getRandomPositiveIntegerWithDecimalPlaces(35.65, 35.7, 5),
-  lng: getRandomPositiveIntegerWithDecimalPlaces(139.7, 139.8, 5),
-};
 
-const OFFER = {
-  title: "Заголовок предложения",
-  address: "" + LOCATION.lat + ", " + LOCATION.lng,
-  price: getRandomPositiveInteger(0, 999999),
-  type: getRandomArrayElement(TYPES),
-  rooms: getRandomPositiveInteger(1, 100),
-  guests: getRandomPositiveInteger(1, 100),
-  checkin: getRandomArrayElement(CHECKINS),
-  checkout: getRandomArrayElement(CHECKOUTS),
-  features: getArrayWithNoRepeat(FEATURES),
-  description: "Описание помещения",
-  photos: getArrayWithNoRepeat(PHOTOS),
-};
+const SIMILAR_AD_LIST =[];
+for (let i = 0; i<10; i++){
+  const AUTHOR = {
+    avatar: `img/avatars/user${getImageId()}.png`,
+  };
+  const LOCATION = {
+    lat: getRandomPositiveIntegerWithDecimalPlaces(35.65, 35.7, 5),
+    lng: getRandomPositiveIntegerWithDecimalPlaces(139.7, 139.8, 5),
+  };
 
-const SIMILAR_AD = {
-  author: AUTHOR,
-  offer: OFFER,
-  location: LOCATION,
-};
+  const OFFER = {
+    title: 'Заголовок предложения',
+    address: `${LOCATION.lat}, ${LOCATION.lng}`,
+    price: getRandomPositiveInteger(0, 999999),
+    type: getRandomArrayElement(TYPES),
+    rooms: getRandomPositiveInteger(1, 100),
+    guests: getRandomPositiveInteger(1, 100),
+    checkin: getRandomArrayElement(CHECKINS),
+    checkout: getRandomArrayElement(CHECKOUTS),
+    features: getArrayWithNoRepeat(FEATURES),
+    description: 'Описание помещения',
+    photos: getArrayWithNoRepeat(PHOTOS),
+  };
 
-console.log(OFFER);
-console.log(LOCATION);
-console.log(SIMILAR_AD);
+  const SIMILAR_AD = {
+    author: AUTHOR,
+    offer: OFFER,
+    location: LOCATION,
+  };
+
+  SIMILAR_AD_LIST.push(SIMILAR_AD);
+}
