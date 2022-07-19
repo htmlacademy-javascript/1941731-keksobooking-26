bb96b19794db30150f33088e0c38f2e2e5d89eda
GurinYI <Gurin.ucheba@gmail.com>
2022-07-19 16:37:06 +0500

    задание 9

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function randomizeFromInterval(min, max) {
+const getRandomPositiveInteger = (min, max) => {
   if (min > max) {
     const temporary = min;
     min = max;
@@ -7,20 +7,96 @@ function randomizeFromInterval(min, max) {
 
   const result = Math.floor(Math.random() * (max - min + 1) + min); //Взял тут https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
   return result;
-}
-randomizeFromInterval(1, 10);
+};
 
-function randomizeFromIntervalWithDecimalPlaces(min, max, decimalPlaces) {
+const getRandomPositiveIntegerWithDecimalPlaces = (min, max, decimalPlaces) => {
   if (min > max) {
     const temporary = min;
     min = max;
     max = temporary;
   }
-
-  const randomizeResult = Math.random() * (max - min + 1) + min;
-  const power = Math.pow(10, decimalPlaces); //Взял тут https://stackoverflow.com/questions/45735472/generate-a-random-number-between-2-values-to-2-decimals-places-in-javascript
-  const result = Math.floor(randomizeResult * power) / power;
+  const precision = Math.pow(10, decimalPlaces);
+  const result =
+    Math.floor((Math.random() * (max - min) + min) * precision) / precision;
   return result;
-}
+};
 
-randomizeFromIntervalWithDecimalPlaces(1, 10, 5);
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomPositiveInteger(0, elements.length - 1)];
+};
+
+const getArrayWithNoRepeat = (elements) => {
+  let randomArray = [];
+  let randomArrayLength = getRandomPositiveInteger(1, elements.length);
+  while (randomArray.length < randomArrayLength) {
+    let randomIndex = getRandomPositiveInteger(0, elements.length - 1);
+    if (!randomArray.includes(elements[randomIndex])) {
+      randomArray.push(elements[randomIndex]);
+    }
+  }
+  return randomArray;
+};
+
+let unusedId = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const getImageId = () => {
+  imageId = getRandomPositiveInteger(1, 10);
+  let index = unusedId.indexOf(imageId);
+  imageId < 10 ? (imageId = "0" + imageId) : (imageId += "");
+  if (index >= 0) {
+    unusedId.splice(index, 1);
+  }
+  console.log(unusedId);
+  return imageId;
+};
+const TYPES = ["palace", "flat", "house", "bungalow", "hotel"];
+
+const PHOTOS = [
+  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg",
+  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg",
+  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg",
+];
+
+const CHECKINS = ["12:00", "13:00", "14:00"];
+
+const CHECKOUTS = ["12:00", "13:00", "14:00"];
+
+const FEATURES = [
+  "wifi",
+  "dishwasher",
+  "parking",
+  "washer",
+  "elevator",
+  "conditioner",
+];
+
+const AUTHOR = {
+  avatar: "img/avatars/user" + getImageId() + ".png",
+};
+const LOCATION = {
+  lat: getRandomPositiveIntegerWithDecimalPlaces(35.65, 35.7, 5),
+  lng: getRandomPositiveIntegerWithDecimalPlaces(139.7, 139.8, 5),
+};
+
+const OFFER = {
+  title: "Заголовок предложения",
+  address: "" + LOCATION.lat + ", " + LOCATION.lng,
+  price: getRandomPositiveInteger(0, 999999),
+  type: getRandomArrayElement(TYPES),
+  rooms: getRandomPositiveInteger(1, 100),
+  guests: getRandomPositiveInteger(1, 100),
+  checkin: getRandomArrayElement(CHECKINS),
+  checkout: getRandomArrayElement(CHECKOUTS),
+  features: getArrayWithNoRepeat(FEATURES),
+  description: "Описание помещения",
+  photos: getArrayWithNoRepeat(PHOTOS),
+};
+
+const SIMILAR_AD = {
+  author: AUTHOR,
+  offer: OFFER,
+  location: LOCATION,
+};
+
+console.log(OFFER);
+console.log(LOCATION);
+console.log(SIMILAR_AD);
