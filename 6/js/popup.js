c0d19b1f526db73f57896c6ef652ff6ecadb7aa8
Keks <keks@htmlacademy.ru>
2022-07-26 14:13:30 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/popup.js
@@ -0,0 +1,65 @@
+import {getSimilarAds} from './data.js';
+
+const similarAdList = getSimilarAds();
+
+const mapCanvas = document.querySelector('#map-canvas');
+const similarAdTemplate = document.querySelector('#card').content.querySelector('.popup');
+
+const typeContainer = document.querySelector('#type');
+const typeList = typeContainer.querySelectorAll('option');
+
+
+const getOfferType = (offerType) => {
+  for (const type of typeList){
+
+    if (type.value === offerType) {
+
+      return  type.textContent;
+    }
+  }
+};
+
+const setTextContent =(node, objectValue) => {
+  if(objectValue === undefined){
+    node.classList.add('visually-hidden');
+  } else {
+    node.textContent = objectValue;
+  }
+};
+
+const setSrcContent =(node, objectValue) => {
+  if(objectValue === undefined){
+    node.classList.add('visually-hidden');
+  } else {
+    node.src = objectValue;
+  }
+};
+similarAdList.forEach((ad) => {
+  const adElement = similarAdTemplate.cloneNode(true);
+  setTextContent(adElement.querySelector('.popup__title'), ad.offer.title);
+  setTextContent(adElement.querySelector('.popup__text--address'), ad.offer.address);
+  setTextContent(adElement.querySelector('.popup__text--price'), ad.offer.price ? `${ad.offer.price} ₽/ночь` : undefined);
+  setTextContent(adElement.querySelector('.popup__type'), ad.offer.type ? getOfferType(ad.offer.type) : undefined);
+  setTextContent(adElement.querySelector('.popup__text--capacity'), (ad.offer.rooms && ad.offer.guests) ? `${ad.offer.rooms} комнаты для ${ad.offer.guests} гостей` : undefined);
+  setTextContent(adElement.querySelector('.popup__text--time'), ad.offer.checkin ? `Заезд после ${ad.offer.checkin}, выезд до ${ad.offer.checkout}` : undefined);
+  setTextContent(adElement.querySelector('.popup__features'), ad.offer.features);
+  setTextContent(adElement.querySelector('.popup__description'), ad.offer.description);
+  adElement.querySelector('.popup__photo').remove();
+
+  if(!(ad.offer.photos.length ===0)){
+
+    for (let i = 0; i< ad.offer.photos.length; i++){
+
+      const photo = document.createElement('img');
+      photo.src = ad.offer.photos[i];
+      photo.width = 45;
+      photo.height = 40;
+      photo.alt ='Фотография жилья';
+      photo.classList.add('popup__photo');
+      adElement.querySelector('.popup__photos').appendChild(photo);
+    }
+  }
+
+  setSrcContent(adElement.querySelector('.popup__avatar'), ad.author.avatar);
+  mapCanvas.appendChild(adElement);
+});
---
+++ 6/js/util.js
@@ -0,0 +1,72 @@
+const getRandomPositiveInteger = (min, max) => {
+  if (min > max) {
+    const temporary = min;
+    min = max;
+    max = temporary;
+  }
+
+  const result = Math.floor(Math.random() * (max - min + 1) + min); //Взял тут https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+  return result;
+};
+
+const getRandomPositiveIntegerWithDecimalPlaces = (min, max, decimalPlaces) => {
+  if (min > max) {
+    const temporary = min;
+    min = max;
+    max = temporary;
+  }
+
+  const BASE = 10;
+  const precision = Math.pow(BASE, decimalPlaces);
+  const result =
+    Math.floor((Math.random() * (max - min) + min) * precision) / precision;
+
+  return result;
+};
+
+function getRandomArrayElement(elements) {
+  return elements[getRandomPositiveInteger(0, elements.length - 1)];
+}
+
+const getArrayWithNoRepeat = (elements) => {
+  const randomArray = [];
+  const randomArrayLength = getRandomPositiveInteger(1, elements.length);
+
+  while (randomArray.length < randomArrayLength) {
+    const randomIndex = getRandomPositiveInteger(0, elements.length - 1);
+
+    if (!randomArray.includes(elements[randomIndex])) {
+      randomArray.push(elements[randomIndex]);
+    }
+  }
+
+  return randomArray;
+};
+
+const unusedId = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const getImageId = () => {
+  const imageId = getRandomPositiveInteger(0, unusedId.length - 1);
+  const index = unusedId.indexOf(unusedId[imageId]);
+  let imageLink = '';
+  const largestSingleNumber = 9;
+  if (unusedId[imageId] <= largestSingleNumber) {
+    imageLink =`0${unusedId[imageId]}`;
+  } else {
+    imageLink = `${unusedId[imageId]}`;
+  }
+
+  if (index >= 0) {
+    unusedId.splice(index,1);
+  }
+
+  return imageLink;
+};
+
+
+export {
+  getRandomPositiveInteger,
+  getRandomPositiveIntegerWithDecimalPlaces,
+  getRandomArrayElement,
+  getArrayWithNoRepeat,
+  getImageId};
